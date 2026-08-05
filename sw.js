7cf43be7e5e4723d8add8fd366a40e241ee3a165
leehaebeen1998-ui <leehaebeen1998@gmail.com>
2026-08-05 16:00:52 +0900

    Refresh Excel background cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mogeom-arcade-v8";
+const CACHE="mogeom-arcade-v9";
 const CORE=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
