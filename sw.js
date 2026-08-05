6664718090acce1f4c00f33406a9daa29405ccd5
leehaebeen1998-ui <leehaebeen1998@gmail.com>
2026-08-05 15:57:50 +0900

    Refresh photo puzzle cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mogeom-arcade-v7";
+const CACHE="mogeom-arcade-v8";
 const CORE=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
